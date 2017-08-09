174e874117bf0a47caf6346beb6fb734d4eede50
anujagarwal93 <anujagarwal.kiit@gmail.com>
2017-08-09 16:03:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,13 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'new value';
 
+var marginLeft = 0;
 var img = document.getElementById('madi');
+function moveRight(){
+  marginLeft = marginLeft + 10;
+  img.stlye.marginLeft = marginLeft + 'px';
+}
 img.onclick = function(){
-  img.style.marginLeft = '100px';  
+    var interval = setInterval(moveRight, 100);
+  //img.style.marginLeft = '100px';  
 };
